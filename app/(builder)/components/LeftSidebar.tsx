8f6b0f4049b275254bd3614213312d14477550dd
Petar Marković <pmarkovic2308@gmail.com>
2025-10-29 13:37:36 +0100

    project init

---
+++ app/(builder)/components/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LeftSidebar() {
+    return (
+        <div></div>
+    );
+}
+
+export default LeftSidebar;
---
+++ app/(builder)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <div>
+        <h1>Beautiful testimonials</h1>
+      </div>
+  );
+}
